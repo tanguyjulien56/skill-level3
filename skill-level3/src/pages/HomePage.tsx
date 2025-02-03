fb4fff8a7bd358d247578f95449b2e28ee6b4583
Julien <tanguyjulien@hotmail.fr>
2025-02-03 14:44:47 +0100

    git commit -m "feat(user): implement dynamic image and birthday countdown with user data"

--- skill-level3/src/pages/HomePage.tsx
+++ skill-level3/src/pages/HomePage.tsx
@@ -1,16 +1,7 @@
-// src/pages/HomePage.tsx
-
 import React from "react";
 import UserProfile from "../components/UserProfileCard";
-import { UserData } from "../types/UserData";
-
+import { userData } from "../data/userData";
 const HomePage: React.FC = () => {
-  const userData: UserData = {
-    firstName: "Juien",
-    lastName: "Doe",
-    birthDate: "1980-06-15", // Date d'anniversaire manquante
-  };
-
   return (
     <div className="App">
       <UserProfile userData={userData} />
