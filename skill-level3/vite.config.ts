2f56f0f1a24e21731fd76432394fb07e7726731d
Julien <tanguyjulien@hotmail.fr>
2025-02-05 11:03:59 +0100

    feat: optimize bundle size by implementing manual chunks in vite.config.ts and lazy loading for the HomePage component

--- skill-level3/vite.config.ts
+++ skill-level3/vite.config.ts
@@ -1,7 +1,19 @@
-// vite.config.js
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Séparer les dépendances tierces (ex : React, Redux, etc.)
+          if (id.includes("node_modules")) {
+            // Crée un chunk pour chaque dépendance de node_modules
+            return id.toString().split("node_modules/")[1].split("/")[0];
+          }
+        },
+      },
+    },
+  },
 });
